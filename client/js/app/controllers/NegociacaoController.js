01857423fd69f22d969d9435339fcec9e725095d
flavio <fdsdev@gmail.com>
2018-08-07 21:34:41 -0300

    criando alias "tipo jquery" com bind do document

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -2,6 +2,14 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        alert("adiciona invocado!");
+
+        let $ = document.querySelector.bind(document);
+        let inputData = $('#data');
+        let inputQuantidade =  $('#quantidade');
+        let inputValor = $('#valor');
+
+        console.log(inputData.value);
+        console.log(inputQuantidade.value);
+        console.log(inputValor.value);
     }
 }
