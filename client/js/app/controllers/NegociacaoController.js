84bcaefa211b969135bcf26f8200677fc0611edf
flavio <fdsdev@gmail.com>
2018-08-08 06:02:20 -0300

    trabalhando com string template (interpolacação)

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -12,13 +12,13 @@ class NegociacaoController {
         event.preventDefault();
 
         let negociacao = new Negociacao(
-            DateHelper().textoParaData(this._inputData.value),
+            DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
             this._inputValor.value
           );
     
           console.log(negociacao);
-          console.log(DateHelper().dataParaTexto(negociacao.data))
+          console.log(DateHelper.dataParaTexto(negociacao.data))
     }
 }
 
