a0461fee0c1bbea89a34ae2e83c209d697374026
flavio <fdsdev@gmail.com>
2018-08-08 05:55:57 -0300

    trabalhando com métodos estáticos

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,10 +1,15 @@
 class DateHelper {
-    textoParaData(dataTexto) {
+
+    constructor() {
+        throw new Error('Esta classe não pode ser instanciada');
+    }
+
+    static textoParaData(dataTexto) {
         //  spread operator -> cada item do array passa a ser um parametro para o new Date
         return new Date(...dataTexto.split('-').map((item,i) => item - i % 2));
     }
 
-    dataParaTexto(data) {
+    static dataParaTexto(data) {
         return data.getDate() + '/' + (data.getMonth() + 1) + '/' + data.getFullYear();
     }
 }
