282773e15bbf83e6a15d12a3323184005917723a
Flávio de Souza <flavio@syncode.com.br>
2018-08-10 14:24:13 -0300

    parseando json da resposta

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -51,7 +51,6 @@ class NegociacaoController {
         xhr.onreadystatechange = () => {
             if(xhr.readyState == 4) {
                 if(xhr.status == 200) {
-                    console.log('Obtendo as negociações do servidor.');
                     JSON.parse(xhr.responseText)
                         .map(objeto=> new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
                         .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao))
