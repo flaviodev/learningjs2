dbbe67e1d4f66aee1034bc3dde07483ee9196f75
Flávio de Souza <flavio@syncode.com.br>
2018-08-13 08:05:22 -0300

    finalizando aula 3 parte 2

--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -1,12 +1,14 @@
 class NegociacaoService {
 
     constructor() {
+
         this._http = new HttpService();
     }
 
     obterNegociacoesDaSemana() {
 
         return new Promise((resolve, reject) => {
+            
             this._http
                 .get('negociacoes/semana')
                 .then(negociacoes => {
@@ -22,7 +24,8 @@ class NegociacaoService {
     obterNegociacoesDaSemanaAnterior() {
 
         return new Promise((resolve, reject) => {
-            this._ttp
+
+            this._http
                 .get('negociacoes/anterior')
                 .then(negociacoes => {
                     resolve(negociacoes.map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor)));
@@ -37,6 +40,7 @@ class NegociacaoService {
     obterNegociacoesDaSemanaRetrasada() {
 
         return new Promise((resolve, reject) => {
+
             this._http
                 .get('negociacoes/retrasada')
                 .then(negociacoes => {
