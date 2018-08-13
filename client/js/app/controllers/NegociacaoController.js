9645153a635427495647a456e57e6d324910313c
Flávio de Souza <flavio@syncode.com.br>
2018-08-13 07:36:01 -0300

    trabalhando com mais de uma requisicao, problema de requisicoes assincronas

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -47,15 +47,33 @@ class NegociacaoController {
     importaNegociacoes() {
 
         let service = new NegociacaoService();
-      
-        service.obterNegociacoesDaSemana((erro,negociacoes) => {
+    
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+    
             if(erro) {
                 this._mensagem.texto = erro;
                 return;
             }
-      
             negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-            this._mensagem.texto = 'Negociações importadas com sucesso';
+    
+            service.obterNegociacoesDaSemanaAnterior((erro, negociacoes) => {
+    
+                if(erro) {
+                    this._mensagem.texto = erro;
+                    return;
+                }
+                negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+    
+                service.obterNegociacoesDaSemanaRetrasada((erro, negociacoes) => {
+    
+                    if(erro) {
+                        this._mensagem.texto = erro;
+                        return;
+                    }
+                    negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações importadas com sucesso';
+                });
+            });
         });
-      }
+    }
 }
