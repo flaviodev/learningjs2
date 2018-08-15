2eeee99f7c363ed09d6df2bf1d35899db80e4da1
Flávio de Souza <flavio@syncode.com.br>
2018-08-15 08:05:20 -0300

    aplicando design pattern module em connection factory para isolar os atributos

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,47 +1,49 @@
-var stores = ['negociacoes'];
-var currentVersion = 1;
-var dbName = 'aluraframe';
+var ConnectionFactory = (function() {
+    let stores = ['negociacoes'];
+    let currentVersion = 1;
+    let dbName = 'aluraframe';
 
-var migrations = [];
-migrations[1] = { version: 1, migration: connection => stores.forEach(store => {
-        if(connection.objectStoreNames.contains(store)) 
-            connection.deleteObjectStore(store);
+    let migrations = [];
+    migrations[1] = { version: 1, migration: connection => stores.forEach(store => {
+            if(connection.objectStoreNames.contains(store)) 
+                connection.deleteObjectStore(store);
 
-        connection.createObjectStore(store, { autoIncrement: true });
-    })};
+            connection.createObjectStore(store, { autoIncrement: true });
+        })};
 
-var connection = null;
+    let connection = null;
 
-class ConnectionFactory {
+    return class ConnectionFactory {
 
-    constructor() {
+        constructor() {
+        
+            throw new Error('Não é possível criar uma instância de Connection Factory');
+        }
+
+        static getConnection() {
+
+            return new Promise((resolve, reject) => {
+
+                let openRequest = window.indexedDB.open(dbName,currentVersion);
+
+                openRequest.onupgradeneeded = e => {
+                    
+                    console.log('Cria ou altera banco existente!');
+                    migrations[currentVersion].migration(e.target.result);
+                };
     
-        throw new Error('Não é possível criar uma instância de Connection Factory');
+                openRequest.onsuccess = e => {
+
+                    if(!connection) connection = e.target.result;
+                    resolve(connection);
+                };
+        
+                openRequest.onerror = e => {
+        
+                    reject(e.target.console.error.name);
+                };
+            });
+        }
+
     }
-
-    static getConnection() {
-
-        return new Promise((resolve, reject) => {
-
-            let openRequest = window.indexedDB.open(dbName,currentVersion);
-
-            openRequest.onupgradeneeded = e => {
-                
-                console.log('Cria ou altera banco existente!');
-                migrations[currentVersion].migration(e.target.result);
-            };
-  
-            openRequest.onsuccess = e => {
-
-                if(!connection) connection = e.target.result;
-                resolve(connection);
-            };
-    
-            openRequest.onerror = e => {
-    
-                reject(e.target.console.error.name);
-            };
-        });
-    }
-
-}
+})();
